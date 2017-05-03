55e819d3e1daff2f478af92dbb2234e1991a3ea2
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-03 15:11:04 -0400

    added eventItem model and partial. Nothing showing...should be able to push cart button at this point.

---
+++ events/Models/EventItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace events.Models
+{
+    public class EventItem
+    {
+        public EventItem()
+        {
+
+        }
+        public EventItem(EventItem s)
+        {
+            this.Name = s.Name;
+            this.Description = s.Description;
+            this.Id = s.Id;
+            this.Price = s.Price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Price { get; set; }
+
+
+        [NotMapped]
+        public Guid TrackerId { get; set; } = Guid.NewGuid();
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ events/Scripts/event.js
@@ -0,0 +1,11 @@
+﻿
+let addToCart = (itemId) => {
+    $.ajax({
+        url: "/home/ShoppingCart/" + itemId,
+        method: "POST",
+        dataType: "html",
+        success: (partial) => {
+            $("#shoppingCart").html(partial);
+        }
+    });
+}
